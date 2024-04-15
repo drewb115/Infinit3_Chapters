3242c79453b96d3899bfa1ad6a049214b04e527b
Drewb115 <Andrewbenedicto05@gmail.com>
2024-04-15 12:14:34 -0500

    Update database schema and add pages migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_08_173501) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_10_125817) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -110,6 +110,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_08_173501) do
   create_table "page_contents", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "page_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
